b59138d8cb53b4d096b85830fdbd5de359277781
Luis Cuervo <luis.cuervo@fortris.com>
2024-11-18 13:15:35 +0100

    add basic examples

--- runbooks/test.sql
+++
@@ -1,7 +0,0 @@
-SELECT customerid, firstname, lastname
-FROM customers
-WHERE customerid = {{ .customer_id
-                    | description "the id of the customer"
-                    | required "customer_id is required"
-                    | type "number"
-                    | squote }}
